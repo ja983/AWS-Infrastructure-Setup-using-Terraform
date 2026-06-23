9eed3bfd53218480f659e052e428462da7a8ea97
ja983 <91011523+ja983@users.noreply.github.com>
2026-06-23 18:31:28 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "web_sg" {
+
+  vpc_id = aws_vpc.devops_vpc.id
+
+
+  ingress {
+
+    from_port = 22
+
+    to_port = 22
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+
+  ingress {
+
+    from_port = 80
+
+    to_port = 80
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+}
